c4f1149c17d2727169efbccefd24d9d1b63f7ec4
roncrisco <roncrisco@3b4f36ec-720f-0410-be2c-c60033042678>
2009-01-16 21:37:05 +0000

    [ 2418388 ] Add silent install scripts

---
+++ ilo_uninstall_silent_sys.sql
@@ -0,0 +1,294 @@
+REM ILO
+REM Copyright (c) 2006 - 2008 by Method R Corporation. All rights reserved.
+REM
+REM This library is free software; you can redistribute it and/or
+REM modify it under the terms of the GNU Lesser General Public
+REM License as published by the Free Software Foundation; either
+REM version 2.1 of the License, or (at your option) any later version.
+REM
+REM This library is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+REM Lesser General Public License for more details.
+REM
+REM You should have received a copy of the GNU Lesser General Public
+REM License along with this library; if not, write to the Free Software
+REM Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+REM
+PROMPT =========================================================================
+PROMPT
+PROMPT This script will UNINSTALL the Instrumentation Library for Oracle
+PROMPT and its associated utilities, including SLA extensions if present.
+PROMPT
+PROMPT This script executes without prompt, for silent installs. One argument is
+PROMPT expected, the schema owner for ILO objects. The script should be executed 
+PROMPT while connected to the SYS account with SYSDBA privileges.
+PROMPT
+PROMPT Objects Uninstalled:
+PROMPT
+PROMPT * For version 2.0 and earlier
+PROMPT
+PROMPT * Package HOTSOS_SYSUTIL 
+PROMPT * Package HOTSOS_ILO_TASK
+PROMPT * Package HOTSOS_ILO_TIMER
+PROMPT * Package SYS.HOTSOS_SYSUTIL (if no other ILO schemas detected)
+PROMPT
+PROMPT * Public Synonym HOTSOS_SYSUTIL 
+PROMPT * Public Synonym HOTSOS_ILO_TASK
+PROMPT * Public Synonym HOTSOS_ILO_TIMER
+PROMPT
+PROMPT * For version 2.1 and later
+PROMPT
+PROMPT * Package ILO_SYSUTIL 
+PROMPT * Package ILO_UTIL 
+PROMPT * Package ILO_TASK
+PROMPT * Package ILO_TIMER
+PROMPT
+PROMPT * Public Synonym ILO_SYSUTIL 
+PROMPT * Public Synonym ILO_UTIL 
+PROMPT * Public Synonym ILO_TASK
+PROMPT * Public Synonym ILO_TIMER
+PROMPT
+PROMPT * Table ILO_RUN
+PROMPT * Table ILO_SCHEDULE_CACHE
+PROMPT
+PROMPT * Sequence ILO_RUN_SEQ
+PROMPT
+PROMPT * Trigger ILO_LOGOFF_TRG
+PROMPT * Trigger SYS.ILO_LOGOFF_TRG (if no other ILO schemas detected)
+PROMPT
+PROMPT =========================================================================
+PROMPT 
+PROMPT
+
+set verify off define '&' 
+PROMPT Using argument 1: &1 as owner of ILO packages
+
+set echo off heading off termout off feedback off
+set serveroutput on size 1000000
+
+rem create the h_user subst variable
+column tempuser new_value h_user
+select '&1' tempuser from dual;
+
+select 'start ' || decode(user, 'SYS', 'ilo_sysok.sql', 'ilo_sysnotok.sql')
+from  dual
+
+spool ilo_uninstall.tmp
+/
+spool off
+start ilo_uninstall.tmp
+
+set echo off heading off termout off feedback off
+select 'start ' || 'ilo_userok.sql'
+from all_users where upper(username) = upper('&&h_user')
+UNION
+select 'start ' || 'ilo_usernotok.sql'
+from dual 
+where not exists (select null from all_users where upper(username) = upper('&&h_user'))
+
+spool ilo_uninstall.tmp
+/
+spool off
+start ilo_uninstall.tmp
+
+set termout on feedback on
+spool ilo_uninstall.log
+PROMPT
+-- only drop the system trigger if no other ILO schema exists
+declare
+  procedure drop_trigger (p_obj varchar2) is
+    not_exists exception;
+    PRAGMA EXCEPTION_INIT (not_exists, -4080);
+  begin
+    for rec in 
+      (select null from dual where not exists
+        (select null from dba_source
+           where name in ('HOTSOS_ILO_TASK','ILO_TASK') 
+             and owner not in ('&&h_user',upper('&&h_user'))
+        )
+       ) 
+    loop
+      dbms_output.put_line('... Dropping trigger '||p_obj);
+      execute immediate 'drop trigger '||p_obj;
+    end loop;
+  exception
+    when not_exists then null;
+    when others then dbms_output.put_line(sqlerrm);
+  end drop_trigger;
+begin
+  drop_trigger('sys.ilo_logoff_trg');
+end;
+/
+PROMPT
+declare
+  procedure drop_trigger (p_obj varchar2) is
+    not_exists exception;
+    PRAGMA EXCEPTION_INIT (not_exists, -4080);
+  begin
+    dbms_output.put_line('... Dropping trigger '||p_obj);
+    execute immediate 'drop trigger '||p_obj;
+  exception
+    when not_exists then null;
+    when others then dbms_output.put_line(sqlerrm);
+  end drop_trigger;
+begin
+  drop_trigger('&&h_user..ilo_logoff_trg');
+end;
+/
+PROMPT
+-- only drop public synonyms if they point to this schema
+declare
+  procedure drop_public_synonym (p_obj varchar2) is
+    not_exists exception;
+    PRAGMA EXCEPTION_INIT (not_exists, -1432);
+  begin
+    for rec in 
+      (select synonym_name from dba_synonyms
+         where table_owner in ('&&h_user',upper('&&h_user'))
+           and owner = 'PUBLIC' and synonym_name = upper(p_obj)
+       ) 
+    loop
+      dbms_output.put_line('... Dropping public synonym '||p_obj);
+      execute immediate 'drop public synonym '||p_obj;
+    end loop;
+  exception
+    when not_exists then null;
+    when others then dbms_output.put_line(sqlerrm);
+  end drop_public_synonym;
+begin
+  drop_public_synonym('hotsos_ilo_task');
+  drop_public_synonym('hotsos_ilo_timer');
+  drop_public_synonym('hotsos_sysutil');
+  drop_public_synonym('ilo_task');
+  drop_public_synonym('ilo_timer');
+  drop_public_synonym('ilo_sysutil');
+  drop_public_synonym('ilo_util');
+end;
+/
+PROMPT
+PROMPT ... Revoking Execute Privileges on &&h_user packages ...
+begin
+  execute immediate 'revoke execute on &&h_user..HOTSOS_ILO_TASK from PUBLIC';
+exception when others then null;
+end;
+/
+begin
+  execute immediate 'revoke execute on &&h_user..HOTSOS_ILO_TIMER from PUBLIC';
+exception when others then null;
+end;
+/
+begin
+  execute immediate 'revoke execute on &&h_user..HOTSOS_SYSUTIL from PUBLIC';
+exception when others then null;
+end;
+/
+begin
+  execute immediate 'revoke execute on &&h_user..ILO_TASK from PUBLIC';
+exception when others then null;
+end;
+/
+begin
+  execute immediate 'revoke execute on &&h_user..ILO_TIMER from PUBLIC';
+exception when others then null;
+end;
+/
+begin
+  execute immediate 'revoke execute on &&h_user..ILO_SYSUTIL from PUBLIC';
+exception when others then null;
+end;
+/
+begin
+  execute immediate 'revoke execute on &&h_user..ILO_UTIL from PUBLIC';
+exception when others then null;
+end;
+/
+PROMPT
+declare
+  procedure drop_package (p_obj varchar2) is
+    not_exists exception;
+    PRAGMA EXCEPTION_INIT (not_exists, -4043);
+  begin
+    dbms_output.put_line('... Dropping package '||p_obj);
+    execute immediate 'drop package '||p_obj;
+  exception
+    when not_exists then null;
+    when others then dbms_output.put_line(sqlerrm);
+  end drop_package;
+begin
+  -- If uninstalling version < 1.6 and no other schemas exist
+  begin
+    for rec in 
+      (select null from dual where not exists
+        (select null from dba_source
+           where name = 'HOTSOS_ILO_TASK' 
+             and owner not in ('&&h_user',upper('&&h_user'))
+        )
+       ) 
+    loop
+      drop_package('sys.HOTSOS_SYSUTIL');
+    end loop;
+  exception when others then null; -- don't break other schema
+  end;
+  drop_package('&&h_user..HOTSOS_ILO_TIMER');
+  drop_package('&&h_user..HOTSOS_ILO_TASK');
+  drop_package('&&h_user..HOTSOS_SYSUTIL');
+  drop_package('&&h_user..ILO_TIMER');
+  drop_package('&&h_user..ILO_TASK');
+  drop_package('&&h_user..ILO_SYSUTIL');
+  drop_package('&&h_user..ILO_UTIL');
+end;
+/
+PROMPT
+PROMPT ... Revoking Execute Privileges on DBMS_MONITOR or DBMS_SUPPORT from &&h_user   
+begin
+  execute immediate 'revoke EXECUTE ON  SYS.DBMS_MONITOR from &&h_user';
+exception when others then null;
+end;
+/
+begin
+  execute immediate 'revoke EXECUTE ON  SYS.DBMS_SUPPORT from &&h_user';
+exception when others then null;
+end;
+/
+PROMPT
+declare
+  procedure drop_table (p_obj varchar2) is
+    not_exists exception;
+    PRAGMA EXCEPTION_INIT (not_exists, -942);
+  begin
+    dbms_output.put_line('... Dropping table '||p_obj);
+    execute immediate 'drop table '||p_obj;
+  exception
+    when not_exists then null;
+    when others then dbms_output.put_line(sqlerrm);
+  end drop_table;
+begin
+  drop_table('&&h_user..ILO_RUN');
+  drop_table('&&h_user..ILO_SCHEDULE_CACHE');
+end;
+/
+PROMPT
+declare
+  procedure drop_sequence (p_obj varchar2) is
+    not_exists exception;
+    PRAGMA EXCEPTION_INIT (not_exists, -2289);
+  begin
+    dbms_output.put_line('... Dropping sequence '||p_obj);
+    execute immediate 'drop sequence '||p_obj;
+  exception
+    when not_exists then null;
+    when others then dbms_output.put_line(sqlerrm);
+  end drop_sequence;
+begin
+  drop_sequence('&&h_user..ILO_RUN_SEQ');
+end;
+/
+PROMPT
+PROMPT =================================================================================
+PROMPT UNINSTALL of ILO Complete
+PROMPT
+PROMPT Note: The &&h_user schema was NOT dropped as part of the uninstall. 
+PROMPT If you wish to do so, copy/paste this:
+PROMPT DROP USER &&h_user CASCADE
+PROMPT =================================================================================
